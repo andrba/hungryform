d1801c9b1b19f170fda1185bb2aad06b1d3edf8d
Andrey <andrey.bazhutkin@gmail.com>
2014-12-15 15:25:40 +1100

    Do not include into to_hash nil params

--- spec/elements/text_field_spec.rb
+++ spec/elements/text_field_spec.rb
@@ -4,4 +4,15 @@ describe HungryForm::TextField do
   it_behaves_like "an active element" do
     let(:active_element_options) { {} }
   end
+
+  let(:resolver) { HungryForm::Resolver.new({}) }
+  let(:group) { HungryForm::Group.new(:group, nil, resolver, {}) {} }
+  let(:element_options) { {} }
+  subject { described_class.new(:element_name, group, resolver, element_options) {} }
+
+  describe "#to_json" do
+  	it "should return a json string" do
+  		expect(subject.to_json).to eq '{"group_element_name": {"visible": "true"}}'
+		end
+	end
 end
