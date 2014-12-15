e3a70871d03b0fedff1be10ac027e127e401403b
Andrey <andrey.bazhutkin@gmail.com>
2014-12-16 10:02:58 +1100

    Form to_json

--- lib/hungryform/elements/select_field.rb
+++ lib/hungryform/elements/select_field.rb
@@ -1,28 +1,28 @@
 class HungryForm
   class SelectField < BaseOptionsElement
-  	attr_accessor :multiple
-  	alias_method :multiple?, :multiple
+    attr_accessor :multiple
+    alias_method :multiple?, :multiple
 
     hashable :multiple
 
-  	def initialize(name, parent, resolver, attributes = {}, &block)
-  		self.multiple = attributes[:multiple] || false
+    def initialize(name, parent, resolver, attributes = {}, &block)
+      self.multiple = attributes[:multiple] || false
       super
-		end
+    end
 
-		# Sets a value of the element
+    # Sets a value of the element
     # Checks the value from the resolver params against the available options
     def set_value
       if resolver.params.has_key?(name)
-      	
-      	# Check if all values present in the options
-      	if multiple?
-      		acceptable_values = (resolver.params[name] - options.keys).empty?
-    		else
-    		 	acceptable_values = options.keys.include?(resolver.params[name])
-  		 	end
+        
+        # Check if all values present in the options
+        if multiple?
+          acceptable_values = (resolver.params[name] - options.keys).empty?
+        else
+          acceptable_values = options.keys.include?(resolver.params[name])
+        end
 
-      	self.value = resolver.params[name] if acceptable_values
+        self.value = resolver.params[name] if acceptable_values
       else
         self.value = @attributes[:value]
       end
