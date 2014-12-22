aee864d98e3f1835a677a52931f670e47cdf196f
Andrey <andrey.bazhutkin@gmail.com>
2014-12-22 15:36:50 +1100

    TextAreaField -> TextArea

--- spec/elements/text_area_spec.rb
+++ spec/elements/text_area_spec.rb
@@ -1,6 +1,6 @@
 require "spec_helper"
 
-describe HungryForm::Elements::TextAreaField do
+describe HungryForm::Elements::TextArea do
   it_behaves_like "an active element" do
     let(:active_element_options) { {} }
   end
