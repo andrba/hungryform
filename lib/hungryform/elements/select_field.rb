9fc66fd53a213eab0590569fec50b322a0a824e9
Andrey <andrey.bazhutkin@gmail.com>
2015-01-21 08:05:16 +1100

    Acceptable values refactoring

--- lib/hungryform/elements/select_field.rb
+++ lib/hungryform/elements/select_field.rb
@@ -5,17 +5,23 @@ module HungryForm
       # Checks the value from the resolver params against the available options
       def set_value
         if resolver.params.key?(name)
-          options_sym_keys = options.keys.map(&:to_sym)
-
-          # Check if all values present in the options
-          if @attributes[:multiple] && resolver.params[name].respond_to?(:map)
-            acceptable_values = (resolver.params[name].map(&:to_sym) - options_sym_keys).empty?
-          else
-            acceptable_values = options_sym_keys.include?(resolver.params[name].to_sym)
-          end
-          self.value = resolver.params[name] if acceptable_values
+          self.value = resolver.params[name] if acceptable_values?
         else
           self.value = attributes.delete(:value)
+        end
+      end
+
+      private
+
+      # Check if all of the values from the resolver params are present
+      # in the options.
+      def acceptable_values?
+        options_sym_keys = options.keys.map(&:to_sym)
+
+        if @attributes[:multiple] && resolver.params[name].respond_to?(:map)
+          (resolver.params[name].map(&:to_sym) - options_sym_keys).empty?
+        else
+          options_sym_keys.include?(resolver.params[name].to_sym)
         end
       end
     end
